5c6136d3aa91fecb32c0a93567a7bb0f3ae8a967
Eduardo6389 <elm6389@icloud.com>
2026-07-09 00:46:03 -0700

    agregar infoDispositivo

---
+++ shared/src/androidMain/kotlin/com/jetbrains/kmpapp/DeviceInfo.android.kt
@@ -0,0 +1,5 @@
+package com.jetbrains.kmpapp
+
+import android.os.Build
+
+actual fun infoDispositivo(): String = Build.MODEL
---
+++ shared/src/iosMain/kotlin/com/jetbrains/kmpapp/DeviceInfo.ios.kt
@@ -0,0 +1,5 @@
+package com.jetbrains.kmpapp
+
+import platform.UIKit.UIDevice
+
+actual fun infoDispositivo(): String = UIDevice.currentDevice.model
