b7c8d6928c5ef75e8c2f86acc1aa1b67917ab78d
Alondra <mrsteve873@gmail.com>
2026-07-09 20:03:49 -0700

    Sesion 2 (Tarea-primer-punto)
    para nuevoUuid() se implemento epochMillis() y una clase Reloj con un método ahora(): Long

--- shared/src/jvmMain/kotlin/com/jetbrains/kmpapp/util/Uuid.jvm.kt
+++ shared/src/jvmMain/kotlin/com/jetbrains/kmpapp/util/Uuid.jvm.kt
@@ -3,3 +3,9 @@ package com.jetbrains.kmpapp.util
 import java.util.UUID
 
 actual fun nuevoUuid(): String = UUID.randomUUID().toString()
+
+actual fun epochMillis(): Long = System.currentTimeMillis()
+
+actual class Reloj {
+    actual fun ahora(): Long = System.currentTimeMillis()
+}
