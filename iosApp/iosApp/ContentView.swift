0598e8de634e5ec22876498de5e783cf401db3b4
Alondra <mrsteve873@gmail.com>
2026-07-30 00:26:02 -0700

    Vista compartida Compose y Switf nativo

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,17 +2,30 @@ import UIKit
 import SwiftUI
 import Shared
 
+// 1. El puente para ver la App de Compose (Compartida)
 struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
         MainViewControllerKt.MainViewController()
     }
-
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 }
 
+// 2. La vista principal con "Dos Caras"
 struct ContentView: View {
     var body: some View {
-        ComposeView()
+        TabView {
+            // Pestaña 1: La App completa hecha en Compose
+            ComposeView()
                 .ignoresSafeArea()
+                .tabItem {
+                    Label("Compose", systemImage: "paintpalette")
+                }
+
+            // Pestaña 2: La pantalla nativa hecha en SwiftUI
+            MovieListView()
+                .tabItem {
+                    Label("Nativo Swift", systemImage: "applelogo")
+                }
+        }
     }
 }
